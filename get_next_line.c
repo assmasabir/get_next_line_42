b156ca0a6ba1970b2898fdd04616c876155ff9fe
asmae <asmasabirpro@gmail.com>
2023-12-22 04:04:50 +0100

    commit

--- get_next_line.c
+++ get_next_line.c
@@ -1,5 +1,6 @@
 #include "get_next_line.h"
 
+
 char *get_next_line(int fd)
 {
     ssize_t res;
@@ -11,55 +12,64 @@ char *get_next_line(int fd)
     static char *reserve;
 
     i = 0;
-    j= 0;
-   
-    buff = (char*)malloc(sizeof(char)*size);
+    j = 0;
+
     if (buff == NULL)
-        return NULL;
-    reserve = (char*)malloc(sizeof(char)*size);
+        buff = (char *)malloc(sizeof(char) * size);
+
     if (reserve == NULL)
+        reserve = (char *)malloc(sizeof(char) * size);
+
+    if (buff == NULL || reserve == NULL)
         return NULL;
-    while (ft_strchr(buff,'\n')==0)
+
+    while ((res = read(fd, buff, size)) > 0)
     {
-        res = read(fd, buff, size);
-        if (res == -1)
-        return (NULL);
         buff[res] = '\0';
-        while(buff[i] != '\0')
+        i = 0;
+        while (buff[i] != '\0')
         {
-            reserve[i]=buff[i];
+            reserve[j] = buff[i];
             i++;
-        }
-        reserve[i]='\0';
-    }
-    temp = (char*)malloc(sizeof(char)*(size+1));
-        if (temp == NULL)
-            return NULL;
-    while (reserve[j] != '\0')
-    { 
-        if (reserve[j] != '\n')
-        {
-            temp[j]= reserve[j];
             j++;
         }
-        else 
+        reserve[j] = '\0';
+
+        if (ft_strchr(buff, '\n') != 0)
+            break;
+    }
+
+    if (res == -1 || (res == 0 && j == 0))
+        return NULL;
+
+    temp = (char *)malloc(sizeof(char) * (j + 1));
+
+    if (temp == NULL)
+        return NULL;
+
+    j = 0;
+    while (reserve[j] != '\0')
+    {
+        temp[j] = reserve[j];
+        if (reserve[j] == '\n')
         {
-            temp[j] = reserve[j];
             j++;
             break;
         }
+        j++;
     }
-    temp[j]= '\0';
+    temp[j] = '\0';
 
- 
-
-    return (temp);
-    
+    return temp;
 }
 int main ()
 {
     int fd = open("example.txt", O_CREAT|O_RDWR);
     printf("%s", get_next_line(fd));
+     printf("%s", get_next_line(fd));
+      printf("%s", get_next_line(fd));
+       printf("%s", get_next_line(fd));
+        printf("%s", get_next_line(fd));
 
 
 }
